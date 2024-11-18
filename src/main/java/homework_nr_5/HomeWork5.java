51edb1cdf1396a3952c78cd72d6c4f149b52c752
olly2810 <aliohinaolga07@gmail.com>
2024-11-18 19:06:06 +0200

    modified number 101

--- src/main/java/homework_nr_5/HomeWork5.java
+++ src/main/java/homework_nr_5/HomeWork5.java
@@ -3,7 +3,7 @@ package homework_nr_5;
 public class HomeWork5 {
     public static void main(String[] args) {
         int[] numbers = new int[101];
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < 101; i++) {
             numbers[i] = i;
             System.out.println(numbers[i]);
 
