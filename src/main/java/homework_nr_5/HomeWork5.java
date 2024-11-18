5b2e069e413463365a89e14b2ba200e2253a272d
olly2810 <aliohinaolga07@gmail.com>
2024-11-18 17:33:06 +0200

    Create an array with name numbers

--- src/main/java/homework_nr_5/HomeWork5.java
+++ src/main/java/homework_nr_5/HomeWork5.java
@@ -2,9 +2,11 @@ package homework_nr_5;
 
 public class HomeWork5 {
     public static void main(String[] args) {
+        int[] numbers = new int[101];
+        for(int i = 100; i < 0; i--) {
+            System.out.println(numbers[i]);
 
-
-
+        }
     };
 }
 
