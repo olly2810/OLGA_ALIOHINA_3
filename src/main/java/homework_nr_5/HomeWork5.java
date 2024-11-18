38b9ea3abff5da10097819fad1dd03188a01fdf8
olly2810 <aliohinaolga07@gmail.com>
2024-11-18 18:20:29 +0200

    Output array to display

--- src/main/java/homework_nr_5/HomeWork5.java
+++ src/main/java/homework_nr_5/HomeWork5.java
@@ -3,7 +3,8 @@ package homework_nr_5;
 public class HomeWork5 {
     public static void main(String[] args) {
         int[] numbers = new int[101];
-        for(int i = 100; i < 0; i--) {
+        for(int i = 0; i < 100; i++) {
+            numbers[i] = i;
             System.out.println(numbers[i]);
 
         }
